530983c10f27cf50005677522e83efc34767bb52
Robert Havelaar <rmhavelaar@gmail.com>
2021-02-11 17:15:10 -0600

    Model added, Index.html updated.

--- Cowsay/Homepage/models.py
+++ Cowsay/Homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CowsayDisplay(models.Model):
+    userInputText = models.CharField(max_length = 100)
+    cowsayDisplay = models.CharField
+    publishDate = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.userInputText
